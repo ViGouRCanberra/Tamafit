833349a4e3d4c5498746659b5797c4f81151b1db
vigour <vigour.canberra@gmail.com>
2018-05-23 21:31:57 +0200

    POC for animation with 2 frames

--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
-/*
- * Entry point for the watch app
- */
+import document from "document";
 
-console.log("App Started");
+let image = document.getElementById('image');
+let animationFrame = 1;
+
+setInterval(swapImage, 500);
+
+function swapImage() {
+    switch(animationFrame) {
+        case 1:
+            image.href = "img/dog1.jpeg";
+            animationFrame = 2;
+            break;
+        case 2:
+            image.href = "img/dog2.jpeg";
+            animationFrame = 1;
+            break;
+    }
+}
